68240f6b3af78f8b5cad7f0dd89dcf47db97ba9f
J-Chandra-py <jayachandraterli@gmail.com>
2025-05-09 23:04:06 +0200

    Add initial setup scripts and requirements for the project

---
+++ commands.sh
@@ -0,0 +1,8 @@
+# Create a new Conda environment named "myenv" with Python 3.6
+conda create -n myenv python=3.6 -y
+
+# Activate the new environment
+conda activate myenv
+
+# Install the required packages from requirements.txt
+pip install -r c:/Projects/SoftwareLab_SS25/Navier_Stokes_cylinder2D/requirements.txt
